cca71abb23d6643f36cec10d67bd0cde6c198bbb
nedelman <nedelman@gmail.com>
2017-06-06 06:08:08 -0700

    Moved clean scripts.

---
+++ clean.bat
@@ -0,0 +1,40 @@
+del cpp\application\project\windows-win32-vs\*.user
+del cpp\application\project\windows-win32-vs\*.suo
+del cpp\application\project\windows-win32-vs\*.sdf
+del cpp\application\project\windows-win32-vs\*.db
+rmdir /S /Q cpp\application\project\windows-win32-vs\bin
+rmdir /S /Q cpp\application\project\windows-win32-vs\ipch
+rmdir /S /Q cpp\application\project\windows-win32-vs\x64
+rmdir /S /Q cpp\application\project\windows-win32-vs\Debug
+rmdir /S /Q cpp\application\project\windows-win32-vs\Release
+rmdir /S /Q cpp\application\project\windows-win32-vs\obj
+rmdir /S /Q "cpp\application\project\windows-uwp-vs\Generated Files"
+rmdir /S /Q cpp\application\project\windows-uwp-vs\x64
+rmdir /S /Q cpp\application\project\windows-uwp-vs\x86
+rmdir /S /Q cpp\application\project\windows-uwp-vs\arm
+
+del cpp\test\project\windows-win32-vs\*.user
+del cpp\test\project\windows-win32-vs\*.suo
+del cpp\test\project\windows-win32-vs\*.sdf
+del cpp\test\project\windows-win32-vs\*.db
+rmdir /S /Q cpp\test\project\windows-win32-vs\bin
+rmdir /S /Q cpp\test\project\windows-win32-vs\ipch
+rmdir /S /Q cpp\test\project\windows-win32-vs\x64
+rmdir /S /Q cpp\test\project\windows-win32-vs\Debug
+rmdir /S /Q cpp\test\project\windows-win32-vs\Release
+rmdir /S /Q cpp\test\project\windows-win32-vs\obj
+
+del cpp\library\project\windows-win32-vs\*.user
+del cpp\library\project\windows-win32-vs\*.suo
+del cpp\library\project\windows-win32-vs\*.sdf
+del cpp\library\project\windows-win32-vs\*.db
+rmdir /S /Q cpp\library\project\windows-win32-vs\bin
+rmdir /S /Q cpp\library\project\windows-win32-vs\ipch
+rmdir /S /Q cpp\library\project\windows-win32-vs\x64
+rmdir /S /Q cpp\library\project\windows-win32-vs\Debug
+rmdir /S /Q cpp\library\project\windows-win32-vs\Release
+rmdir /S /Q cpp\library\project\windows-win32-vs\obj
+rmdir /S /Q "cpp\library\project\windows-uwp-vs\Generated Files"
+rmdir /S /Q cpp\library\project\windows-uwp-vs\x64
+rmdir /S /Q cpp\library\project\windows-uwp-vs\x86
+rmdir /S /Q cpp\library\project\windows-uwp-vs\arm
---
+++ clean.sh
@@ -0,0 +1,10 @@
+rm -r cpp/library/project/linux-netbeans/build
+rm -r cpp/library/project/linux-netbeans/dist
+
+rm -r cpp/library/project/ios-xcode/Build
+rm -r cpp/library/project/macos-xcode/Build
+rm -r cpp/library/project/tvos-xcode/Build
+
+rm -r cpp/library/project/ios-xcode/finjin-engine-lib.xcodeproj/xcuserdata
+rm -r cpp/library/project/macos-xcode/finjin-engine-lib.xcodeproj/xcuserdata
+rm -r cpp/library/project/tvos-xcode/finjin-engine-lib.xcodeproj/xcuserdata
